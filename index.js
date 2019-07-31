0af253697c47a4ad36828b444058678d4c97ebae
zhangnan <zhangnan35@foxmail.com>
2019-07-31 17:31:43 +0800

    添加了ssl证书

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Koa = require("koa");
 const app = new Koa();
 const connect = require("./config/connect");
+const https = require("https");
+const fs = require("fs");
 const bodyParser = require("koa-bodyparser")();
 const cors = require("koa2-cors")();
 const apiMiddleware = require("./config/api");
 const koajwt = require("koa-jwt");
+const sslOption = {
+  key: fs.readFileSync("./config/tencent.key"),
+  cert: fs.readFileSync("./config/tencent.crt")
+};
 
 (async () => {
   await connect();
@@ -12,12 +18,16 @@ const koajwt = require("koa-jwt");
   app.use(cors);
   app.use(
     koajwt({ secret: "my-token" }).unless({
-      path: [/\/register/, /\/login/, /\/qiniuToken/,/\/posts/,/\/queries/]
+      path: [/\/register/, /\/login/, /\/qiniuToken/, /\/posts/, /\/queries/]
     })
   );
   app.use(apiMiddleware);
 })();
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//   console.log("koa服务已启动");
+// });
+
+https.createServer(sslOption, app.callback()).listen(3000, () => {
   console.log("koa服务已启动");
 });
